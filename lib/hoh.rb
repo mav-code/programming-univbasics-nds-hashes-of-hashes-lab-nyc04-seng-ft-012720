c3103b26587bc3ee17fd6e8c5c5f5310a39e413c
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-19 00:49:48 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,7 +38,16 @@ def naming_system
     sub_category: {
       label: "Class",
       sub_category : {
-        label: 
+        label: "Order",
+        sub_category: {
+          label: "Family",
+          sub_category: {
+            label: "Genus",
+            sub_category: {
+              label: "Species",
+            }
+          }
+        }
       }
     }
   }
